07f29ac404ef8a474ef3274fa670d0f2fbcbcaef
Teja Sundeep <14033164+tejasundeep@users.noreply.github.com>
2023-08-31 13:19:06 +0530

    Update candlestick_pattern_detector.py

--- candlestick_pattern_detector.py
+++ candlestick_pattern_detector.py
@@ -1,56 +1,70 @@
 import yfinance as yf
 import pandas as pd
 
-# Download historical stock data
-symbol = "AAPL"
-data = yf.download(symbol, interval='5m', period="7d")
+def download_data(symbol: str, interval: str = "1m", period: str = "7d") -> pd.DataFrame:
+    try:
+        return yf.download(symbol, interval=interval, period=period)
+    except Exception as e:
+        print(f"An error occurred while downloading data: {e}")
+        return pd.DataFrame()
 
-# Calculate candle body and wick sizes
-data['Body'] = abs(data['Close'] - data['Open'])
-data['Wick'] = data['High'] - data['Low'] - data['Body']
+def build_pattern(window: pd.DataFrame) -> str:
+    pattern_list = [
+        "G" if window['Close'].iloc[i] > window['Open'].iloc[i] else "R"
+        for i in range(len(window))
+    ]
+    return "".join(pattern_list)
 
-# Calculate median body and wick sizes
-median_body = data['Body'].median()
-median_wick = data['Wick'].median()
-
-# Categorize each candle based on body size, wick size, and color
-def categorize_candle(row):
-    body_size = 'L' if row['Body'] >= median_body else 'S'
-    wick_size = 'L' if row['Wick'] >= median_wick else 'S'
-    color = 'G' if row['Close'] > row['Open'] else 'R'
-    return f"{body_size}{color}{wick_size}W"
-
-# Classify each candle
-data['Category'] = data.apply(categorize_candle, axis=1)
-
-# Initialize accuracy counts
-accuracy_counts = {}
-
-# Find patterns with high accuracy
-for i in range(len(data) - 1):
-    current_candle = data['Category'].iloc[i]
-    next_move = data['Category'].iloc[i + 1]
+def find_custom_patterns(data: pd.DataFrame, min_window_size: int, min_occurrences: int, accuracy_threshold: float) -> dict:
+    if data.empty:
+        print("No data to process.")
+        return {}
     
-    if current_candle not in accuracy_counts:
-        accuracy_counts[current_candle] = {'Counts': {}, 'Total': 0}
-    
-    if next_move not in accuracy_counts[current_candle]['Counts']:
-        accuracy_counts[current_candle]['Counts'][next_move] = 0
-    
-    accuracy_counts[current_candle]['Counts'][next_move] += 1
-    accuracy_counts[current_candle]['Total'] += 1
+    accuracy_counts = {}
+    high_accuracy_patterns = {}
 
-high_accuracy_patterns = {}
+    for window_size in range(min_window_size, min_window_size * 2):
+        for i in range(0, len(data) - window_size):
+            window = data.iloc[i:i + window_size]
+            pattern = build_pattern(window)
+            next_move = 'U' if data['Close'].iloc[i + window_size] > window['Close'].iloc[-1] else 'D'
 
-# Calculate accuracy
-for pattern, data in accuracy_counts.items():
-    total = data['Total']
-    for next_move, count in data['Counts'].items():
-        accuracy = count / total
-        if accuracy > 0.85:
-            high_accuracy_patterns[f"{pattern}->{next_move}"] = {'Count': count, 'Accuracy': accuracy * 100}
+            accuracy_counts.setdefault(pattern, {'U': 0, 'D': 0, 'Total': 0})
+            accuracy_counts[pattern][next_move] += 1
+            accuracy_counts[pattern]['Total'] += 1
 
-# Print high accuracy patterns
-print("High accuracy patterns:")
-for pattern, stats in high_accuracy_patterns.items():
-    print(f"Pattern: {pattern}, Count: {stats['Count']}, Accuracy: {stats['Accuracy']:.2f}%")
+    for pattern, counts in accuracy_counts.items():
+        if counts['Total'] >= min_occurrences:
+            accuracy = max(counts['U'], counts['D']) / counts['Total']
+            if accuracy >= accuracy_threshold:
+                high_accuracy_patterns[pattern] = counts
+                high_accuracy_patterns[pattern]['Accuracy'] = accuracy * 100
+
+    return high_accuracy_patterns
+
+def predict_next_move(data: pd.DataFrame, high_accuracy_patterns: dict, min_window_size: int) -> None:
+    last_window = data.iloc[-min_window_size:]
+    last_pattern = build_pattern(last_window)
+
+    if last_pattern in high_accuracy_patterns:
+        likely_move = 'U' if high_accuracy_patterns[last_pattern]['U'] > high_accuracy_patterns[last_pattern]['D'] else 'D'
+        accuracy = high_accuracy_patterns[last_pattern]['Accuracy']
+        print(f"Next likely move: {likely_move}, Accuracy: {accuracy}%")
+    else:
+        print("No pattern found.")
+
+if __name__ == "__main__":
+    symbol = "AAPL"
+    data = download_data(symbol)
+    min_window_size = 5
+    min_occurrences = 5
+    accuracy_threshold = 0.95
+
+    high_accuracy_patterns = find_custom_patterns(data, min_window_size, min_occurrences, accuracy_threshold)
+
+    print("High accuracy patterns:")
+    for pattern, counts in high_accuracy_patterns.items():
+        print(f"Pattern: {pattern}, Counts: {counts}, Accuracy: {counts['Accuracy']:.2f}%")
+
+    print("Predicting the next move based on the last pattern:")
+    predict_next_move(data, high_accuracy_patterns, min_window_size)
